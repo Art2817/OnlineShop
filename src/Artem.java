f5a3dc69473728b90a893746bb4ffba4d8dfd8ab
Artem <artem.cherednychenko2817@gmail.com>
2025-09-30 13:17:12 +0200

    2 commit

--- src/Artem.java
+++ src/Artem.java
@@ -2,6 +2,6 @@ public class Artem
 {
     public static void main(String[] args)
     {
-    System.out.println("This is shop-online app");
+    System.out.println("Shop-online for customers");
     }
  }
